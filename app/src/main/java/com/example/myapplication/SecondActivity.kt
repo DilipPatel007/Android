a52f4f5919c5db7476c9d4d96024a476372a232a
Dilip Patel <pateldilip2902@gmail.com>
2024-03-18 16:57:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val userName = intent.getStringExtra("USER")
+        val textView = findViewById<TextView>(R.id.tvOffers)
+        val message = "$userName, you will get one month free subscription for free."
+        textView.text = message
+    }
+}
