35793523b1e49d59037d6e02a68b3529d1794d31
Dilip Patel <pateldilip2902@gmail.com>
2024-03-26 12:58:15 +0530

    Lottie Animation Demo

---
+++ LottieAnimationDemo/app/src/main/java/com/example/lottieanimationdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lottieanimationdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView laView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        laView.setAnimation(R.raw.lottie);
+//        laView.playAnimation();
+//        laView.loop(true);
+    }
+}
