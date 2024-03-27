98c3a06b56cecb2a8db1f3ce9dd3d530e86d230f
Dilip Patel <pateldilip2902@gmail.com>
2024-03-27 12:03:47 +0530

    RecyclerViewDemo

---
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.recyclerviewdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Create an ArrayList to store contact data
+    ArrayList<ContactModel> arrContacts = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the RecyclerView in the layout by its ID
+        RecyclerView recyclerView = findViewById(R.id.recyclerContact);
+
+        // Set a LinearLayoutManager for the RecyclerView
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Add sample contact data to the ArrayList
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "A", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "B", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "C", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "D", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "E", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "F", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "G", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "H", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "I", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "J", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "K", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "L", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "M", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "N", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "O", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "P", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "Q", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "R", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "S", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "T", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "U", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "V", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "W", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "X", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "Y", "954585665"));
+        arrContacts.add(new ContactModel(R.drawable.ic_launcher_foreground, "Z", "954585665"));
+
+        // Create an adapter and pass the context and contact data
+        RecyclerContactAdapter adapter = new RecyclerContactAdapter(this, arrContacts);
+
+        // Set the adapter for the RecyclerView
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/RecyclerContactAdapter.java
@@ -0,0 +1,113 @@
+package com.example.recyclerviewdemo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * This class creates an adapter to be used with a RecyclerView to display a list of contacts.
+ */
+public class RecyclerContactAdapter extends RecyclerView.Adapter<RecyclerContactAdapter.ViewHolder> {
+
+    /**
+     * The context of the application.
+     */
+    Context context;
+
+    /**
+     * An ArrayList of ContactModel objects that represents the data to be displayed in the RecyclerView.
+     */
+    ArrayList<ContactModel> arrContacts;
+
+    /**
+     * This constructor initializes the context and the ArrayList of ContactModel objects.
+     *
+     * @param context  The context of the application.
+     * @param arrContacts The ArrayList of ContactModel objects containing the contact data.
+     */
+    RecyclerContactAdapter(Context contect, ArrayList<ContactModel> arrContacts){
+        this.context = contect;
+        this.arrContacts = arrContacts;
+    }
+
+    /**
+     * This method is called by the RecyclerView layout manager to create a new ViewHolder.
+     *
+     * @param parent   The ViewGroup that holds the RecyclerView.
+     * @param viewType  The view type of the new view.
+     * @return A new ViewHolder that holds the views for a single contact item.
+     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.contact_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    /**
+     * This method is called by the RecyclerView to bind the data to the views of a ViewHolder at a specific position.
+     *
+     * @param holder   The ViewHolder that holds the views for the contact item.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imgContact.setImageResource(arrContacts.get(position).img);
+        holder.txtName.setText(arrContacts.get(position).name);
+        holder.txtNumber.setText(arrContacts.get(position).number);
+
+    }
+
+    /**
+     * This method returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in the ArrayList of ContactModel objects.
+     */
+    @Override
+    public int getItemCount() {
+        return arrContacts.size();
+    }
+
+    /**
+     * This class represents a single ViewHolder that holds the views for a contact item.
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView txtName, txtNumber;
+        ImageView imgContact;
+
+        /**
+         * This constructor initializes the views for the contact item.
+         *
+         * @param itemView The view that represents a single contact item.
+         */
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtName = itemView.findViewById(R.id.txtName);
+            txtNumber = itemView.findViewById(R.id.txtNumber);
+            imgContact = itemView.findViewById(R.id.imgContact);
+        }
+    }
+
+
+}
+
+
+//        This Java class is an adapter for a RecyclerView in an Android app.
+//        It populates the RecyclerView with a list of contacts (ContactModel) and displays their names, phone numbers, and images.
+//        Let’s go through the key parts:
+//        Constructor: The constructor initializes the adapter with a context and an array of ContactModel objects.
+//        onCreateViewHolder: Inflates the layout for each contact row (R.layout.contact_row) and creates a new ViewHolder.
+//        onBindViewHolder: Binds data (contact image, name, and number) to the ViewHolder.
+//        getItemCount: Returns the total number of contacts in the list.
+//        ViewHolder class: Holds references to UI elements (text views and image view) within each contact row.
+
