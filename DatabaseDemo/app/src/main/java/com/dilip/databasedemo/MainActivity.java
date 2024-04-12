c11ecd2a2ef5ce9f06dd4b4d281aebc38811e8fd
Dilip Patel <pateldilip2902@gmail.com>
2024-04-12 12:59:55 +0530

    DatabaseDemo

--- DatabaseDemo/app/src/main/java/com/dilip/databasedemo/MainActivity.java
+++ DatabaseDemo/app/src/main/java/com/dilip/databasedemo/MainActivity.java
@@ -2,24 +2,26 @@ package com.dilip.databasedemo;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        MyDBHelper dbHelper = new MyDBHelper(this);
+
+        dbHelper.addContact("Dilip", "5556454656");
+        dbHelper.addContact("Raman", "4545451545");
+        dbHelper.addContact("Darshan", "31433444");
+        dbHelper.addContact("Vraj", "6848768482");
+        dbHelper.addContact("Dhruv", "6449599455");
+
+
 
     }
 }
--- DatabaseDemo/app/src/main/java/com/dilip/databasedemo/MyDBHelper.java
+++ DatabaseDemo/app/src/main/java/com/dilip/databasedemo/MyDBHelper.java
@@ -1,5 +1,6 @@
 package com.dilip.databasedemo;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,19 +11,46 @@ public class MyDBHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "ContactsDB";
     private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_CONTACT = "Contacts";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PHONE_NUMBER = "phone_no";
 
     public MyDBHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        // Create the Contacts table
+        db.execSQL("CREATE TABLE " + TABLE_CONTACT + "(" +
+                KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                KEY_NAME + " TEXT, " +
+                KEY_PHONE_NUMBER + " TEXT" +
+                ")");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop the existing table and recreate it
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACT);
+        onCreate(db);
+    }
 
+    public void addContact(String name, String phone_no) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name);
+        values.put(KEY_PHONE_NUMBER, phone_no);
+
+        // Insert the contact into the table
+        db.insert(TABLE_CONTACT, null, values);
     }
 }
+
+
+//        The MyDBHelper class extends SQLiteOpenHelper and manages the database creation and version management.
+//        In onCreate, we create the Contacts table with columns for id, name, and phone_no.
+//        In onUpgrade, we drop the existing table if it exists and recreate it.
+//        The addContact method inserts a new contact into the table.
