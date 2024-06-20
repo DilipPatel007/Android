f072b246085287bb865b3a30f0de822de4328c2b
Dilip Patel <pateldilip2902@gmail.com>
2024-06-20 15:48:11 +0530

    initial commit - constructor injection (manual)

---
+++ DaggerExample/app/src/main/java/com/dilip/daggerexample/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.dilip.daggerexample
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userRepository = UserRepository()
+        val emailService = EmailService()
+
+        val userRegistrationService = UserRegistrationService(userRepository, emailService)
+        userRegistrationService.registerUser("aaa@gmail.com", "1234")
+
+    }
+}
